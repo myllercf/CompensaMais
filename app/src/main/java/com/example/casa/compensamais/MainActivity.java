3f2bbf38d2292d153652bcbe6b45111ca161bc39
Myller Claudino de Freitas <myllercf@gmail.com>
2016-11-26 18:09:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/casa/compensamais/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.casa.compensamais;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText qtde1;
+    private EditText valor1;
+    private EditText qtde2;
+    private EditText valor2;
+    private Button button;
+    private TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+
+                qtde1 = (EditText) findViewById(R.id.qtde1);
+                valor1 = (EditText)findViewById(R.id.valor1);
+
+                qtde2 = (EditText)findViewById(R.id.qtde2);
+                valor2 = (EditText)findViewById(R.id.valor2);
+
+                resultado = (TextView) findViewById(R.id.resultado);
+
+                Produto p1 = new Produto();
+                p1.setQtde( Integer.parseInt( qtde1.getText().toString() ) );
+                p1.setValor( Float.valueOf( valor1.getText().toString() ) );
+
+                Produto p2 = new Produto();
+                p2.setQtde( Integer.parseInt( qtde2.getText().toString() ) );
+                p2.setValor( Float.valueOf( valor2.getText().toString() ) );
+
+                resultado.setText(calcular(p1, p2));
+            }
+        });
+
+    }
+
+    public StringBuilder calcular (Produto p1, Produto p2) {
+        StringBuilder resp = new StringBuilder();
+        float custoUnitario1 = p1.getValor() / p1.getQtde();
+        float custoUnitario2 = p2.getValor() / p2.getQtde();
+        DecimalFormat decimal = new DecimalFormat("0.00");
+
+        if ( custoUnitario1 < custoUnitario2 ) {
+            resp.append("Leve a opção com ");
+            resp.append(p1.getQtde());
+            resp.append(". \n");
+            resp.append("Cada item sai ao custo de: R$ ");
+            resp.append(decimal.format(custoUnitario1));
+            resp.append("\n");
+            resp.append("Enquanto comprando ");
+            resp.append(p2.getQtde());
+            resp.append(" sairia por: R$ ");
+            resp.append(decimal.format(custoUnitario2));
+        }
+        else if ( custoUnitario1 > custoUnitario2 ) {
+            resp.append("Compensa comprar mais. \n");
+            resp.append("Levando ");
+            resp.append(p2.getQtde());
+            resp.append(" Cada um sai ao custo de: R$ ");
+            resp.append(decimal.format(custoUnitario2));
+        }
+        else {
+            resp.append("Valores iguais. Veja se vale a pena comprar tudo de uma vez.");
+        }
+
+        return resp;
+    }
+}
---
+++ app/src/main/java/com/example/casa/compensamais/Produto.java
@@ -0,0 +1,26 @@
+package com.example.casa.compensamais;
+
+/**
+ * Created by casa on 22/11/2016.
+ */
+
+public class Produto {
+    private int qtde;
+    private float valor;
+
+    public int getQtde() {
+        return qtde;
+    }
+
+    public void setQtde(int qtde) {
+        this.qtde = qtde;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
